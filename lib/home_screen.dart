4144d07e452df63cae5d679e480bf5c63b9b4187
Muhammad Usman <163582723+musman023@users.noreply.github.com>
2024-04-07 00:20:26 +0500

    set device tokens

---
+++ lib/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_notifications/notification_services.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  NotificationServices notificationServices = NotificationServices();
+
+  @override
+  void initState() {
+    super.initState();
+    notificationServices.requestNotificationPermission();
+
+    // notificationServices.isTokenRefresh();
+
+    notificationServices.getDeviceToken().then((value) {
+      print("Device tokenq: ");
+      print(value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown.shade400,
+        centerTitle: true,
+        title: const Text(
+          "Firebase Notifications",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
